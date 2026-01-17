c5586374b57465cd4756816fddce3372b61da8bb
CJPoppaDoppa <164113849+laurendt@users.noreply.github.com>
2026-01-17 17:26:58 -0500

    Button binding for intake. Sim works

--- src/main/java/frc/robot/Subsystems/Intake/IntakeIOSim.java
+++ src/main/java/frc/robot/Subsystems/Intake/IntakeIOSim.java
@@ -1,11 +1,11 @@
 package frc.robot.Subsystems.Intake;
 
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
 public class IntakeIOSim implements IntakeIO {
   private DCMotorSim motor =
@@ -14,36 +14,28 @@ public class IntakeIOSim implements IntakeIO {
           DCMotor.getKrakenX60(1));
   private double currentVoltage = 0;
 
-  private SimpleMotorFeedforward ffmodel = new SimpleMotorFeedforward(
-    IntakeConstants.SIM.KS,
-    IntakeConstants.SIM.KG,
-    IntakeConstants.SIM.KV
-  );
+  private SimpleMotorFeedforward ffmodel =
+      new SimpleMotorFeedforward(
+          IntakeConstants.SIM.KS, IntakeConstants.SIM.KG, IntakeConstants.SIM.KV);
 
-  private final TrapezoidProfile.Constraints constraints = 
-    new TrapezoidProfile.Constraints(
-      IntakeConstants.SIM.MAX_VELOCITY, 
-      IntakeConstants.SIM.MAX_ACCELERATION
-    );
+  private final TrapezoidProfile.Constraints constraints =
+      new TrapezoidProfile.Constraints(
+          IntakeConstants.SIM.MAX_VELOCITY, IntakeConstants.SIM.MAX_ACCELERATION);
 
-  private final PIDController controller = new PIDController(
-    IntakeConstants.SIM.KP,
-    IntakeConstants.SIM.KI,
-    IntakeConstants.SIM.KD
-  );
+  private final PIDController controller =
+      new PIDController(IntakeConstants.SIM.KP, IntakeConstants.SIM.KI, IntakeConstants.SIM.KD);
 
   @Override
   public void setGoal(double velocity) {
     goal = new TrapezoidProfile.State(0, velocity);
-    setpoint = new TrapezoidProfile.State(
-      motor.getAngularPositionRotations(),
-      motor.getAngularVelocityRPM()
-    );
-  } 
+    setpoint =
+        new TrapezoidProfile.State(
+            motor.getAngularPositionRotations(), motor.getAngularVelocityRPM());
+  }
 
   private final TrapezoidProfile profile = new TrapezoidProfile(constraints);
-    private TrapezoidProfile.State goal = new TrapezoidProfile.State();
-    private TrapezoidProfile.State setpoint = new TrapezoidProfile.State();
+  private TrapezoidProfile.State goal = new TrapezoidProfile.State();
+  private TrapezoidProfile.State setpoint = new TrapezoidProfile.State();
 
   @Override
   public void setVoltage(double voltage) {
@@ -53,11 +45,10 @@ public class IntakeIOSim implements IntakeIO {
 
   @Override
   public void updateMotionProfile() {
-      setpoint = profile.calculate(0.02, setpoint, goal);
-      setVoltage(
+    setpoint = profile.calculate(0.02, setpoint, goal);
+    setVoltage(
         ffmodel.calculateWithVelocities(motor.getAngularVelocityRPM(), setpoint.velocity)
-        + controller.calculate(motor.getAngularPositionRotations(), setpoint.position
-      ));
+            + controller.calculate(motor.getAngularPositionRotations(), setpoint.position));
   }
 
   @Override
