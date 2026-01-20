85d6ed08d8243cc4fedf22e399b4af5c5aa386d6
vramaiah <11704123+vramaiah@users.noreply.github.com>
2026-01-20 17:43:51 -0500

    docs: Document `Intake` subsystem

--- src/main/java/frc/robot/Subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake/Intake.java
@@ -8,10 +8,21 @@ public class Intake extends SubsystemBase {
   public IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
   public IntakeIO io;
 
-  public static Intake getInstance() {
+  /**
+   * Gets an instance of the {@link Intake} subsystem
+   *
+   * @return An instance of the {@link Intake} subsystem, or `null` if none exists
+   */
+  private static Intake getInstance() {
     return instance;
   }
 
+  /**
+   * Gets an instance of the {@link Intake} subsystem. If none exists, this creates one
+   *
+   * @param io
+   * @return An instance of the {@link Intake} subsystem
+   */
   public static Intake Initialize(IntakeIO io) {
     if (instance == null) {
       instance = new Intake(io);
@@ -19,16 +30,25 @@ public class Intake extends SubsystemBase {
     return instance;
   }
 
-  public void setIntakeVoltage(double volts) {
+  /**
+   * Sets the voltage of the intake
+   *
+   * @param volts
+   */
+  public void setVoltage(double volts) {
     io.setVoltage(volts);
   }
 
-  /** Sets velocity, in RPM */
+  /**
+   * Sets velocity of the intake, in RPM using Feedforward and PID
+   *
+   * @param rpm Desired velocity in RPM
+   */
   public void setVelocity(double rpm) {
     io.setGoal(rpm);
   }
 
-  public Intake(IntakeIO io) {
+  private Intake(IntakeIO io) {
     this.io = io;
   }
 
