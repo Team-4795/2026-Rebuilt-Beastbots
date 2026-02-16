424cdd776a1d79c7161911825d74a5cdea5ad929
vramaiah <11704123+vramaiah@users.noreply.github.com>
2026-02-16 17:02:53 -0500

    refactor: Make led set color methods take a Color instead of hsv values

--- src/main/java/frc/robot/Subsystems/Led/Led.java
+++ src/main/java/frc/robot/Subsystems/Led/Led.java
@@ -1,85 +1,76 @@
 package frc.robot.Subsystems.Led;
 
-import java.util.Map;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class Led {
 
-    private enum BlinkState {
-        RAPID, 
-        SLOW,
-        SOLID
+  private enum BlinkState {
+    RAPID,
+    SLOW,
+    SOLID
+  }
+
+  private AddressableLED led;
+  private AddressableLEDBuffer buffer;
+  private BlinkState blink;
+
+  private static Led instance;
+
+  /**
+   * Gets the current instance of {@link Led}. If none exists, it it created
+   *
+   * @return
+   */
+  public static Led getInstance() {
+    if (instance == null) {
+      instance = new Led();
     }
+    return instance;
+  }
 
-    private AddressableLED led;
-    private AddressableLEDBuffer buffer;
-    private BlinkState blink;
+  private Led() {
+    led = new AddressableLED(LedConstants.ledPort);
+    buffer = new AddressableLEDBuffer(LedConstants.ledLength);
+    led.setLength(buffer.getLength());
+  }
 
-    private static Led instance;
+  /**
+   * Sets the color of the LED for a specified range
+   *
+   * @param color A color
+   * @param start Start of the range
+   * @param end End of the range
+   * @see setColor
+   */
+  public void setColor(Color color, int start, int end) {
+    setColorNoOutput(color, start, end);
+    setOutput();
+  }
 
-    /**
-     * Gets the current instance of {@link Led}. If none exists, it it created
-     * @return
-     */
-    public static Led getInstance() {
-        if (instance == null) {
-            instance = new Led();
-        }
-        return instance;
+  /**
+   * Sets the color of the LED *buffer* for a specified range. This does not flush the LED buffer to
+   * the physical LED; make sure to call {@link #setOutput()}.
+   *
+   * @param color The color to set
+   * @see setColor
+   */
+  public void setColorNoOutput(Color color, int start, int end) {
+    start = MathUtil.clamp(start, 0, LedConstants.ledLength);
+    end = MathUtil.clamp(end, start, LedConstants.ledLength);
+
+    for (int i = start; i < end; i++) {
+      buffer.setLED(i, color);
     }
+  }
 
-    private Led() {
-        led = new AddressableLED(LedConstants.ledPort);
-        buffer  = new AddressableLEDBuffer(LedConstants.ledLength);
-        led.setLength(buffer.getLength());
-    }
-    
-    /**
-     * Sets the color of the LED for a specified range
-     * @param h Hue of color
-     * @param s Saturation of color
-     * @param v Value of color
-     * @param start Start of the range
-     * @param end End of the range
-     * @see setColor 
-     */
-    public void setColor(int h, int s, int v, int start, int end) {
-        setColorNoOutput(h, s, v, start, end);
-        setOutput();
-    }
-
-    /**
-     * Sets the color of the LED *buffer* for a specified range. This does not flush the LED buffer to the physical LED; make sure to call {@link #setOutput()}.
-     * @param h Hue of color
-     * @param s Saturation of color
-     * @param v Value of color
-     * @param start Start of the range
-     * @param end End of the range
-     * @see setColor 
-     */
-
-    public void setColorNoOutput(int h, int s, int v, int start, int end) {
-        start = MathUtil.clamp(start, 0, LedConstants.ledLength);
-        end = MathUtil.clamp(end, start, LedConstants.ledLength);
-
-        for(int i = start; i < end; i ++) {
-        buffer.setHSV(i, h, s, v);
-        }
-    }
-
-    /**
-     * Flushes the LED buffer to the physical LED
-     */
-    public void setOutput() {
-        led.setData(buffer);
-        led.start();
-    }
+  /** Flushes the LED buffer to the physical LED */
+  public void setOutput() {
+    led.setData(buffer);
+    led.start();
+  }
 }
-
--- src/main/java/frc/robot/Subsystems/Led/LedConstants.java
+++ src/main/java/frc/robot/Subsystems/Led/LedConstants.java
@@ -3,9 +3,12 @@ package frc.robot.Subsystems.Led;
 import edu.wpi.first.wpilibj.util.Color;
 
 public class LedConstants {
-    public static final int ledPort = 9;
-    public static final int ledLength = 60;
-    
-    private Color colorTeal = Color.kTeal;
-    private Color colorLime = Color.kLimeGreen;
+  public static final int ledPort = 9;
+  public static final int ledLength = 60;
+  public static final int ledStart = 1;
+
+  private Color colorTeal = Color.kTeal;
+  private Color colorLime = Color.kLimeGreen;
+
+  public static final Color teamColor = Color.kLimeGreen;
 }
