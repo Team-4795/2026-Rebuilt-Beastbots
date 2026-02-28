1784c33bcc7dab347bf97622c5a9f303a52f7758
Nicholas-Biggs <nicobiggs23@gmail.com>
2026-02-28 17:27:04 -0500

    Tweaked blink states
    LED sim still needs work

--- src/main/java/frc/robot/Subsystems/Led/Led.java
+++ src/main/java/frc/robot/Subsystems/Led/Led.java
@@ -1,13 +1,13 @@
 package frc.robot.Subsystems.Led;
 
-import java.util.Map;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.Map;
 
 public class Led extends SubsystemBase {
 
@@ -17,12 +17,10 @@ public class Led extends SubsystemBase {
     SOLID
   }
 
-  private Led LED;
   private AddressableLED led;
   private AddressableLEDBuffer buffer;
   private BlinkState blink;
-
-  private static Led instance;
+  private static Led LED;
 
   /**
    * Gets the current instance of {@blink Led}. If none exists, one is created
@@ -30,33 +28,62 @@ public class Led extends SubsystemBase {
    * @return
    */
   public static Led getInstance() {
-    if (instance == null) {
-      instance = new Led();
+    if (LED == null) {
+      LED = new Led();
     }
-    return instance;
+    return LED;
   }
 
   private Led() {
     led = new AddressableLED(LedConstants.ledPort);
     buffer = new AddressableLEDBuffer(LedConstants.ledLength);
     led.setLength(buffer.getLength());
+    getInstance();
 
-    setDefaultCommand(Commands.either(Commands.runOnce(() -> setTeamColors(), LED), 
-    Command.select(Map.ofEntries(
-      Map.entry(null, null),
-      Map.entry(null, null),
-      Map.entry(null, null))), 
-    null));
+    setDefaultCommand(
+        Commands.either(
+                Commands.runOnce(() -> setTeamColors(), LED),
+                Commands.select(
+                    (Map.ofEntries(
+                        Map.entry(BlinkState.RAPID, setABeautifulHue()),
+                        Map.entry(BlinkState.SLOW, blink(0.20, 184, 100, 84)),
+                        Map.entry(BlinkState.SOLID, blink(0.10, 184, 100, 84)))),
+                    () -> blink),
+                DriverStation::isDisabled)
+            .ignoringDisable(true));
+  }
+
+  public Command setABeautifulHue() {
+    return Commands.runOnce(() -> setColor(184, 100, 84, 0, LedConstants.ledLength), LED);
+  }
+
+  public Command setCitrus() {
+    return Commands.runOnce(() -> setColor(72, 77, 86, 0, LedConstants.ledLength), LED);
+  }
+
+  public Command blinkTeamColors(double interrupt, int h, int s, int v) {
+    return Commands.sequence(
+        runOnce(() -> setABeautifulHue()),
+        Commands.waitSeconds(interrupt),
+        runOnce(() -> setCitrus()),
+        Commands.waitSeconds(interrupt));
   }
 
   /** Sets the led strip to the team color */
   private void setTeamColors() {
-    setColorNoOutput(180, 80, 80, 0, (LedConstants.ledLength / 2));
-    setColorNoOutput(90, 100, 100, (LedConstants.ledLength / 2 + 1), LedConstants.ledLength);
+    setColorNoOutput(184, 100, 84, 0, (LedConstants.ledLength / 2));
+    setColorNoOutput(72, 77, 86, ((LedConstants.ledLength / 2) + 1), LedConstants.ledLength);
 
     setOutput();
   }
 
+  public Command blink(double interrupt, int h, int s, int v) {
+    return Commands.sequence(
+        runOnce(() -> setColor(h, s, v, 0, LedConstants.ledLength)),
+        Commands.waitSeconds(interrupt),
+        runOnce(() -> setColor(h, s, v, 0, LedConstants.ledLength)),
+        Commands.waitSeconds(interrupt));
+  }
   /**
    * Sets the color of the LED for a specified range
    *
--- src/main/java/frc/robot/Subsystems/Led/LedConstants.java
+++ src/main/java/frc/robot/Subsystems/Led/LedConstants.java
@@ -5,6 +5,7 @@ public class LedConstants {
   public static final int ledLength = 60;
   public static final int ledStart = 1;
 
-  // TEAL: h = 180, s = 80, v = 80
-  // LIME: h = 90, s = 100, v = 100
+  // TEAL: h = 184, s = 100, v = 84
+  // LIME: h = 72, s = 77, v = 86
+  // AG: h = 262, s = 33, v = 81
 }
